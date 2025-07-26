31771bad7690592ea37bc2c91863a349a4816f66
Walleo <walleo114514@outlook.com>
2025-07-26 12:16:42 +0800

    change master to main

--- src/main/java/cn/walleo/block/SLDoor.java
+++ src/main/java/cn/walleo/block/SLDoor.java
@@ -5,7 +5,6 @@ import cn.walleo.secretlab;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
